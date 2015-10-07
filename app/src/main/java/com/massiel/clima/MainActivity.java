9c4acc97a74d8db444117f3a99c52f5042f167e7
patri19 <payerdis@Macbooks-MacBook-Pro.local>
2015-10-06 18:24:29 -0600

    Initial commit

---
+++ app/src/main/java/com/massiel/clima/MainActivity.java
@@ -0,0 +1,16 @@
+package com.massiel.clima;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by payerdis on 10/6/15.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
