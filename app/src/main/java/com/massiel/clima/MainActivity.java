6fbd4ac329d8512235774e744b6b3ada63d82008
patri19 <payerdis@Macbooks-MacBook-Pro.local>
2015-10-06 18:57:29 -0600

    Tarea completada Listener/recursos

--- app/src/main/java/com/massiel/clima/MainActivity.java
+++ app/src/main/java/com/massiel/clima/MainActivity.java
@@ -2,6 +2,10 @@ package com.massiel.clima;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by payerdis on 10/6/15.
@@ -12,5 +16,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        Button buttonBuscar = (Button) findViewById(R.id.button_buscar);
+
+        buttonBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText_ciudad = (EditText) findViewById(R.id.editText_ciudad);
+                String ciudad = editText_ciudad.getText().toString();
+
+                Toast.makeText(MainActivity.this,ciudad, Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
 }
+
